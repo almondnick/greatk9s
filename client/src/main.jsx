0f123ca10285ee22752937213fcdf92e3f95ccdf
spadilla512 <spadilla512@gmail.com>
2024-03-11 20:51:18 -0500

    updated src files

---
+++ client/src/main.jsx
@@ -0,0 +1,42 @@
+import { ReactDOM } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import BookNow from './pages/BookNow';
+import Services from './pages/Services';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/aboutus',
+                element: <AboutUs />
+            },
+            {
+                path: '/booknow',
+                element: <BookNow />
+            },
+            {
+                path: '/services',
+                element: <Services />
+            }
+        ]
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
