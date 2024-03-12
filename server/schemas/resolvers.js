5606bbba970969ba8facc4373d02284facb73bd0
Michael Kickham <mfkickham3@gmail.com>
2024-03-11 19:06:51 -0500

    added usermodel, typedefs, schema, authjs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,33 @@
 // Resolvers
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Mutation: {
+        addUser: async (parent, args) => {
+          const user = await User.create(args);
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          }
+        }
+      };
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,23 @@
 // Type Defs
+const typeDefs = `
+type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    phoneNumber: String
+    petName: String
+}
+
+type Auth {
+    token: ID
+    user: User
+}
+
+type Mutation {
+    addUser (firstName: String!, lastName: String!, email: String!, password: String!, phoneNumber, petName): Auth
+    login(email: String!, password: String!): Auth
+}
+`;
+
+module.export = typeDefs
