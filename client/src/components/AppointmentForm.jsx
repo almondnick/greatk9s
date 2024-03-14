63c5f61cbfde4adf1c5beef1b8c66750ccd92c8b
almondnick <nickalmond14@gmail.com>
2024-03-13 21:25:02 -0500

    book form

--- client/src/components/AppointmentForm.jsx
+++ client/src/components/AppointmentForm.jsx
@@ -4,6 +4,8 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 function AppointmentForm() {
   const [validated, setValidated] = useState(false);
@@ -22,72 +24,55 @@ function AppointmentForm() {
     <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
+          <Form.Label>First Name</Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="First name"
-            defaultValue="Mark"
+            defaultValue="First Name"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Last name</Form.Label>
+          <Form.Label>Last Name</Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="Last name"
-            defaultValue="Otto"
+            defaultValue="Last Name"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Username</Form.Label>
+          <Form.Label>Email</Form.Label>
           <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+            <InputGroup.Text id="inputGroupPrepend"></InputGroup.Text>
             <Form.Control
               type="text"
-              placeholder="Username"
+              placeholder="Email@email.com"
               aria-describedby="inputGroupPrepend"
               required
             />
             <Form.Control.Feedback type="invalid">
-              Please choose a username.
+              Please enter your email.
             </Form.Control.Feedback>
           </InputGroup>
         </Form.Group>
       </Row>
       <Row className="mb-3">
         <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid zip.
-          </Form.Control.Feedback>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Select Service
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Sitting</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Training</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
         </Form.Group>
       </Row>
-      <Form.Group className="mb-3">
-        <Form.Check
-          required
-          label="Agree to terms and conditions"
-          feedback="You must agree before submitting."
-          feedbackType="invalid"
-        />
-      </Form.Group>
       <Button type="submit">Submit form</Button>
     </Form>
   );
