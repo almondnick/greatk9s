3360b6f6a8dac0672c0d870e3226335a5c1f1a39
almondnick <nickalmond14@gmail.com>
2024-03-13 21:00:24 -0500

    styling and components

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,81 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 const Header = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setProfile] = useState('');
+
+  useEffect(() => {
+
+    setLoggedIn(Auth.loggedIn());
+
+    if (Auth.loggedIn()) {
+      const user = Auth.getProfile();
+      setProfile(user.data.firstName); 
+      console.log(user)
+    }
+  }, []);
+
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+    setLoggedIn(false);
+    setProfile('');
+  };
+
   return (
-    <>
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="/">Great K9s</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Services</Nav.Link>
-            <Nav.Link href="#pricing">About Us</Nav.Link>
-            <Nav.Link href="/booknow">Book Now</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      </>
+    <header>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/">
+              <h1>Home</h1>
+            </Link>
+          </li>
+          <li>
+            <Link to="/aboutus">
+              <h1>About Us</h1>
+            </Link>
+          </li>
+          <li>
+            <Link to="/booknow">
+              <h1>BookNow</h1>
+            </Link>
+          </li>
+          <li>
+            <Link to="/services">
+              <h1>Services</h1>
+            </Link>
+          </li>
+          {loggedIn ? (
+            <>
+              <li>
+                <span>Hello, {username}</span>
+              </li>
+              <li>
+                <button onClick={logout}>
+                  <h1>Logout</h1>
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">
+                  <h1>Login</h1>
+                </Link>
+              </li>
+              <li>
+                <Link to="/signup">
+                  <h1>Sign Up!</h1>
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
   );
 };
 
