16cbb190533ed11fd52af5989e6fae755eb2ada5
Matt R <mattrallo2012@gmail.com>
2024-03-14 17:39:57 -0500

    added to forms to match the models

--- client/src/components/AppointmentForm.jsx
+++ client/src/components/AppointmentForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
@@ -6,9 +6,14 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import Dropdown from 'react-bootstrap/Dropdown';
 
+import TimePicker from 'react-bootstrap-time-picker';
 
 function AppointmentForm() {
   const [validated, setValidated] = useState(false);
+
+  const [service, setService] = useState('Select Another Service'); // Service is selected on dropdown
+  const [serviceCheck, setServiceCheck] = useState(true);
+
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -20,26 +25,36 @@ function AppointmentForm() {
     setValidated(true);
   };
 
+  const checkService = (e) => {
+    setService(e.target.innerText);
+    if(e.target.innerText == 'Sitting'){
+      setServiceCheck(true);
+    }
+    else {
+      setServiceCheck(false);
+    }
+  }
+
   return (
     <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First Name</Form.Label>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Phone Number</Form.Label>
           <Form.Control
             required
             type="text"
-            placeholder="First name"
-            defaultValue="First Name"
+            placeholder="Phone Number"
+            defaultValue="Phone Number"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Last Name</Form.Label>
+          <Form.Label>Pet Name</Form.Label>
           <Form.Control
             required
             type="text"
-            placeholder="Last name"
-            defaultValue="Last Name"
+            placeholder="Pet name"
+            defaultValue="Pet Name"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
@@ -58,30 +73,96 @@ function AppointmentForm() {
             </Form.Control.Feedback>
           </InputGroup>
         </Form.Group>
+      <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>Any Comments? Special Instructions?</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Enter comments here"
+            defaultValue="Enter comments here"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+      </Form.Group>
       </Row>
       <Row className="mb-3">
         <Form.Group as={Col} md="6" controlId="validationCustom03">
           <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Select Service
+            {service}
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Sitting</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Training</Dropdown.Item>
+            <Dropdown.Item href="#/action-1" onClick={checkService}>Sitting</Dropdown.Item>
+            <Dropdown.Item href="#/action-2" onClick={checkService}>Training</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
         </Form.Group>
-      </Row>
-      <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>Start Date</Form.Label>
-          <Form.Control
-            required
-            type="date"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+      
+
+
+      {serviceCheck ? ( // If true then have sitting 
+      <div>
+        <h1>Dog Sitting Selected</h1>
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+        <Form.Label>Start Date</Form.Label>
+        <Form.Control
+          required
+          type="date"
+        />
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+        <Form.Label>End Date</Form.Label>
+        <Form.Control
+          required
+          type="date"
+        />
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>Select Start Time</Form.Label>
+          <TimePicker start="00:00" end="23:30" step={30} />
+
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+
+        </Form.Group>
+
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+        <Form.Label>Select End Time</Form.Label>
+            <TimePicker start="00:00" end="23:30" step={30} />
+
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+
+        </Form.Group>
+        </div>
+      ) : ( // if false then have training
+      <div>
+      <h1>Dog / People Training Selected</h1>
+      <Form.Group as={Col} md="4" controlId="validationCustom01">
+      <Form.Label>Date</Form.Label>
+      <Form.Control
+        required
+        type="date"
+      />
+      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>Select Time</Form.Label>
+          <TimePicker start="00:00" end="23:30" step={30} />
+
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+
+        </Form.Group>
+      </div>
+      )}
+
+     
+      </Row>
       <Button type="submit">Submit form</Button>
+    
+
     </Form>
   );
 }
