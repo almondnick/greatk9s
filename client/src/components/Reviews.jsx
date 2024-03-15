dc5f325f92dd0901bbc05027f6e4bc948ce6797c
Peyton Weber <weberpeyton11@gmail.com>
2024-03-15 17:33:37 -0500

    More updates to front end

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,44 +1,44 @@
 import styled from 'styled-components';
 import Card from 'react-bootstrap/Card';
 
-const Reviews = ()=>  {
-  return (
-    <Wrapper>
-        <Title>Testimonials</Title>
-        <StyledCard>
-        <Card.Header>Five Stars!</Card.Header>
-        <Card.Body>
-            <blockquote className="blockquote mb-0">
-            <p>
-                {' '}
-                The most amazing experience I have ever had. They watched my dog for 6 months and asked no questions.
-                {' '}
-            </p>
-            <footer className="blockquote-footer">
-                Matt Rallo, <cite title="Source Title">St. Louis, MO</cite>
-            </footer>
-            </blockquote>
-        </Card.Body>
-        </StyledCard>
-        <br></br>
-        <StyledCard>
-        <Card.Header>I was blown away.</Card.Header>
-        <Card.Body>
-            <blockquote className="blockquote mb-0">
-            <p>
-                {' '}
-                After just one hour of training, my dog was able to cook a full three course meal for me and my family.
-                {' '}
-            </p>
-            <footer className="blockquote-footer">
-                Sarah Padilla, <cite title="Source Title">Kansas City, MO</cite>
-            </footer>
-            </blockquote>
-        </Card.Body>
-        </StyledCard>
-    </Wrapper>
-  );
-}
+const Reviews = () => {
+    return (
+        <Wrapper>
+            <Title>
+                <u>Testimonials</u>
+            </Title>
+            <StyledCard>
+                <Card.Header style={{ fontSize: "larger" }}>Five Stars!</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {" "}
+                            The most amazing experience I have ever had. They watched my dog for 6 months and asked no questions.{" "}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Matt Rallo, <cite title="Source Title">St. Louis, MO</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </StyledCard>
+            <br></br>
+            <StyledCard>
+                <Card.Header style={{ fontSize: "larger" }}>I was blown away.</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {" "}
+                            After just one hour of training, my dog was able to cook a full three course meal for me and my family.{" "}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Sarah Padilla, <cite title="Source Title">St. Louis, MO</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </StyledCard>
+        </Wrapper>
+    );
+};
 
 export default Reviews;
 
@@ -47,13 +47,19 @@ const Wrapper = styled.div`
     padding-top: 20px;
     padding-bottom: 20px;
     background-color: #92a8d1;
-`
+`;
 
 const Title = styled.h1`
     text-align: center;
     margin-bottom: 20px;
     font-family: baskerville, serif;
-`
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 2px 2px 5px #111111;
+    max-width: 75%;
+    margin-left: auto;
+    margin-right: auto;
+`;
 
 const StyledCard = styled(Card)`
     width: 70%;
@@ -62,4 +68,4 @@ const StyledCard = styled(Card)`
     margin-left: auto;
     font-family: baskerville, serif;
     box-shadow: 1px 1px 10px #111111;
-`
+`;
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,7 +1,40 @@
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import styled from 'styled-components';
+
+// Styled components for SignupForm
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  font-family: baskerville, serif;
+  text-decoration: underline;
+`;
+
+const StyledForm = styled(Form)`
+  font-family: baskerville, serif;
+  text-align: center;
+  display: inline-block;
+  border: 2px solid #111111;
+  border-radius: 4px;
+  width: 40%;
+  background-color: #92a8d1;
+  padding: 25px;
+  box-shadow: 1px 1px 7px #111111;
+  margin-bottom: 15px;
+`;
+
+const StyledInput = styled(Form.Control)`
+  max-width: 50%;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+`;
 
 function SignupForm(props) {
   const [formState, setFormState] = useState({ firstName: '', lastName: '', email: '', password: '' });
@@ -29,61 +62,40 @@ function SignupForm(props) {
       });
       const token = data.addUser.token;
       Auth.login(token);
-    }catch (err) {
+    } catch (err) {
       console.error(err);
     }
-
   };
 
   return (
-    <div className="container my-1">
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+    <Wrapper>
+      <Title>Signup</Title>
+      <StyledForm onSubmit={handleFormSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicFirstName">
+          <Form.Label htmlFor='firstName'>First Name:</Form.Label>
+          <StyledInput type="text" placeholder="First" name="firstName" id="firstName" onChange={handleChange} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicLastName">
+          <Form.Label htmlFor='lastName'>Last Name:</Form.Label>
+          <StyledInput type="text" placeholder="Last" name="lastName" id="lastName" onChange={handleChange} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label htmlFor='email'>Email:</Form.Label>
+          <StyledInput type="email" placeholder="youremail@test.com" name="email" id="email" onChange={handleChange} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label htmlFor='pwd'>Password:</Form.Label>
+          <StyledInput type="password" placeholder="******" name="password" id="pwd" onChange={handleChange} />
+        </Form.Group>
+
+        <Button variant="dark" type="submit">
+          Submit
+        </Button>
+      </StyledForm>
+    </Wrapper>
   );
 }
 
